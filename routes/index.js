203598b5a241beb1f4adf3e234d2a05d745af4f7
Cache Staheli <cache@byu.edu>
2016-04-08 19:27:26 -0600

    files updated

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var SpotifyWebApi = require('spotify-web-api-node');
-require('../smartspot.js');
+var SmartSpot = require('../SmartSpot');
 
 var clientId = 'de9c4a4a601a43a093584aefeef9b845';
 var clientSecret = '3039cd179c4d4673b04aa3cfda9c9bf1';
 var redirectUri = 'http://localhost:3001/callback';
 
-// credentials are optional
-var spotifyApi = new SpotifyWebApi({
-    clientId:clientId,
-    clientSecret:clientSecret,
-    redirectUri:redirectUri
-});
-
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
-var generateRandomString = function(length)
-{
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (var i = 0; i < length; i++)
-    {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-};
+var smartSpot = new SmartSpot(clientId, clientSecret, redirectUri);
 
 router.get('/login', function(req, res, next)
 {
-    var state = generateRandomString(16);
-    var scopes = ['playlist-modify-private', 'playlist-modify-public', 'playlist-read-private'];
-    var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+    smartSpot.login(req, res, next);
+});
 
-    console.log(authorizeURL);
-    res.redirect(authorizeURL);
+router.get('/create', function(req, res, next)
+{
+    //does the actual playlist creation.
 });
 
 router.get('/search', function(req, res, next)
 {
+    //smartSpot.getArtistID(req.) // TODO get the artists name from the request.
     res.render('index', { title:'Express' });
 });
 
--- smartspot.js
+++ smartspot.js
@@ -1,3 +1,138 @@
-var SpotifyWebApi = require('spotify-web-api-node');
+/**
+ * Taps into the Spotify API to create a playlist with top songs from artists most like a certain artist.
+ * @param {string} clientId the client ID code given to the user by Spotify.
+ * @param {string} clientSecret the client secret code given to the user by Spotify.
+ * @param {string} redirectUri a Redirect URI that has been white-listed by Spotify.
+ * @constructor creates a SmartSpot that can access the Spotify API.
+ */
+function SmartSpot(clientId, clientSecret, redirectUri)
+{
+    var SpotifyWebApi = require('spotify-web-api-node');
 
-var spotifyApi = new SpotifyWebApi();
+    var spotifyApi = new SpotifyWebApi({
+        clientId:clientId,
+        clientSecret:clientSecret,
+        redirectUri:redirectUri
+    });
+
+    /**
+     * Generates a random string containing numbers and letters
+     * @param  {number} length The length of the string
+     * @return {string} The generated string
+     */
+    var generateRandomString = function(length)
+    {
+        var text = '';
+        var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        for (var i = 0; i < length; i++)
+        {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    };
+
+    this.login = function(req, res, next)
+    {
+        var state = generateRandomString(16);
+        var scopes = ['playlist-modify-private', 'playlist-modify-public', 'playlist-read-private'];
+        var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+        console.log(authorizeURL);
+        res.redirect(authorizeURL);
+    };
+
+    /**
+     * Gets the artist ID that most closely matches the given artist name.
+     * @param {string} name the name of the artist to search for.
+     */
+    this.getArtistID = function(name)
+    {
+        spotifyApi.searchArtists(name)
+            .then(function(data)
+            {
+                console.log('Search artists for ' + name, data.body);
+            }, function(err)
+            {
+                console.error(err);
+            });
+
+    };
+
+    /**
+     * Gets the artists related to the given artist.
+     * @param {string} artistID the Spotify ID of the artist.
+     */
+    this.getRelatedArtists = function(artistID)
+    {
+        artistId = '0qeei9KQnptjwb8MgkqEoy'; //TODO just for testing
+
+        spotifyApi.getArtistRelatedArtists(artistId)
+            .then(function(data)
+            {
+                if (data.body.artists.length)
+                {
+                    // Print the number of similar artists
+                    console.log('I got ' + data.body.artists.length + ' similar artists!');
+
+                    console.log('The most similar one is ' + data.body.artists[0].name);
+                }
+                else
+                {
+                    console.log('I didn\'t find any similar artists.. Sorry.');
+                }
+
+            }, function(err)
+            {
+                console.log('Something went wrong..', err.message);
+            });
+    };
+
+    /**
+     * Gets the top tracks from the given artist.
+     * @param {string} artistId the id of the artist.
+     */
+    this.getArtistTopTracks = function(artistId)
+    {
+        artistId = '0oSGxfWSnnOXhD2fKuz2Gy';
+        spotifyApi.getArtistTopTracks(artistId, 'US')
+            .then(function(data)
+            {
+                console.log(data.body);
+            }, function(err)
+            {
+                console.log('Something went wrong!', err);
+            });
+    };
+
+    /**
+     * Creates a playlist for the given user with the given title and songs.
+     * @param {string} user the username of the person creating the playlist.
+     * @param {string} playlistTitle the title of the playlist.
+     * @param {Array.<string>} tracks the tracks in the playlist. This should be an array of the form <code>["spotify:track:id", "spotify:track:id", ...]</code>.
+     */
+    this.createPlaylist = function(user, playlistTitle, tracks)
+    {
+        // Create a private playlist
+        var userID = spotifyApi.spotifyApi.createPlaylist('thelinmichael', 'My Cool Playlist', { 'public':false })
+            .then(function(data)
+            {
+                console.log('Created playlist!');
+            }, function(err)
+            {
+                console.log('Something went wrong!', err);
+            });
+
+        // Add tracks to a playlist
+        spotifyApi.addTracksToPlaylist('thelinmichael', '5ieJqeLJjjI8iJWaxeBLuK', ["spotify:track:4iV5W9uYEdYUVa79Axb7Rh", "spotify:track:1301WleyT98MSxVHPZCA6M"])
+            .then(function(data)
+            {
+                console.log('Added tracks to playlist!');
+            }, function(err)
+            {
+                console.log('Something went wrong!', err);
+            });
+    };
+}
+
+module.exports = SmartSpot;
